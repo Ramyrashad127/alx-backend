6b5873eda8b628f81db5b9d290a8717148245a92
Ramyrashad127 <ramy41599@gmail.com>
2024-09-13 14:36:42 +0300

    new

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+
+"""typing for annotation"""
+from typing import Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """return start index and end index for page"""
+    start_in = (page - 1) * page_size
+    end_in = (page - 1) * page_size - 1
+    return (start_in, end_in)
