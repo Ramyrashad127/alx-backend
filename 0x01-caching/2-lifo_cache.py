232432483fdd1839bdfc57ddce87b5787bc44557
Ramyrashad127 <ramy41599@gmail.com>
2024-09-13 20:43:56 +0300

    new

---
+++ 0x01-caching/2-lifo_cache.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+
+"""import modules"""
+from base_caching import BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    """child caching class"""
+    def __init__(self):
+        """constructor of class"""
+        super().__init__()
+        self.stack = []
+
+    def put(self, key, item):
+        """assign item to dictionary"""
+        if key is None or item is None:
+            return
+        if key in self.cache_data:
+            self.cache_data[key] = item
+        else:
+            if len(self.cache_data) == BaseCaching.MAX_ITEMS:
+                remove = self.stack.pop()
+                print(f"DISCARD: {remove}")
+                self.cache_data.pop(remove)
+                self.cache_data[key] = item
+                self.stack.append(key)
+            else:
+                self.cache_data[key] = item
+                self.stack.append(key)
+
+    def get(self, key):
+        """get item from cachie"""
+        if key is None:
+            return None
+        if key not in self.cache_data:
+            return None
+        return self.cache_data[key]
