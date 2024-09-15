5aacb89c7c1f42553bae3a57ca488247e377236d
Ramyrashad127 <ramy41599@gmail.com>
2024-09-15 23:37:37 +0300

    new

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -3,13 +3,17 @@
 
 """import modules"""
 from flask import Flask, render_template
+from typing import Any
 
 
 app = Flask(__name__)
+
+
 @app.route('/')
-def hmoe():
+def hmoe() -> Any:
     """diplay home page"""
     return render_template('0-index.html')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
