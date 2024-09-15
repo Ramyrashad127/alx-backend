3e747cc1cfebae2fb036c40a33f16fc93956c689
Ramyrashad127 <ramy41599@gmail.com>
2024-09-16 00:46:58 +0300

    new

---
+++ 0x02-i18n/4-app.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+
+
+"""import modules"""
+from flask import Flask, render_template, request
+from flask_babel import Babel
+from typing import Any
+
+
+class Config:
+    """Configuration class for app"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+app.url_map.strict_slashes = False
+
+
+babel = Babel(app)
+
+
+@app.route('/')
+def hmoe() -> Any:
+    """diplay home page"""
+    return render_template('3-index.html')
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """get locale language"""
+    locale = request.args.get('locale')
+    if locale in app.config['LANGUAGES']:
+        return locale
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
